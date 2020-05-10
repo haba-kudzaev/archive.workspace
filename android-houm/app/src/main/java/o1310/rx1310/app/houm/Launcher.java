beb1253098cbc6292b28b8ad8f38d8174d47cc6e
rx1310 <rx1310@inbox.ru>
2020-05-10 06:57:51 +0300

    houm: Add files via upload

---
+++ android-houm/app/src/main/java/o1310/rx1310/app/houm/Launcher.java
@@ -0,0 +1,21 @@
+package o1310.rx1310.app.houm;
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Launcher extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.launcher);
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+			Window w = getWindow();
+			w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+		}
+		
+	}
+	
+}
