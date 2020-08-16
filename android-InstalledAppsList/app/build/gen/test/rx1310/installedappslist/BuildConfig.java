5e619b8719b8234f902e7fd6e14322478ae436e0
rx1310 <rx1310@inbox.ru>
2020-08-16 11:56:40 +0300

    InstalledAppsList: Add files

---
+++ android-InstalledAppsList/app/build/gen/test/rx1310/installedappslist/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package test.rx1310.installedappslist;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- android-QuickAccess/app/src/main/java/o1310/rx1310/app/quickaccess/MainActivity.java
+++ android-QuickAccess/app/src/main/java/o1310/rx1310/app/quickaccess/MainActivity.java
@@ -1,16 +1,48 @@
 package o1310.rx1310.app.quickaccess;
 
-import android.app.*;
-import android.os.*;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+import android.preference.PreferenceScreen;
+import android.widget.ListView;
 
-public class MainActivity extends Activity
-{
+public class MainActivity extends PreferenceActivity {
 
-	@Override
-	public void onCreate(Bundle savedInstanceState)
-	{
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+	SharedPreferences s;
+	ListView mListView;
+
+	protected void onCreate(Bundle b) {
+		super.onCreate(b);
+
+		addPreferencesFromResource(R.layout.activity_settings);
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		mListView = findViewById(android.R.id.list);
+		mListView.setDivider(null);
+		s = PreferenceManager.getDefaultSharedPreferences(this);
+		
 	}
 	
+	@Override
+	protected void onResume() {
+		super.onResume();
+	}
+
+	// обработка нажатий на пункты
+	public boolean onPreferenceTreeClick(PreferenceScreen s, Preference p) {
+
+		switch (p.getKey()) {
+
+			// переход в меню "Дополнительное"
+			case "goToAdditionallyActivity": 
+				//startActivity(new Intent(this, AdditionallyActivity.class)); // вызов активности
+				break;
+
+		}
+
+		return super.onPreferenceTreeClick(s, p);
+
+	}
+
 }
